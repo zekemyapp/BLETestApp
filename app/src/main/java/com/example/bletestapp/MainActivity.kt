151165581161c116d726e093a7ce88f519501b12
Juan Ruiz <jprn91@gmail.com>
2019-05-03 02:30:29 +0200

    find ble

--- app/src/main/java/com/example/bletestapp/MainActivity.kt
+++ app/src/main/java/com/example/bletestapp/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.bletestapp
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private val REQUEST_ENABLE_BT = 1
+
+    private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+
+    private val BluetoothAdapter.isDisabled: Boolean
+        get() = !isEnabled
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        bluetoothAdapter?.takeIf { it.isDisabled }?.apply {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
     }
 }
